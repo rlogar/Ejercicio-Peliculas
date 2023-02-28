9bdbbf0e6798b7dda7a996740623e98edbfa0bb9
rlogar <rociolopezgarcia2@gmail.com>
2023-02-28 19:14:00 +0100

    Entendiendo enlaces TMDB

---
+++ js/peliculas.js
@@ -0,0 +1,4 @@
+let populares = "https://api.themoviedb.org/3/movie/popular?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
+let cartelera = "https://api.themoviedb.org/3/movie/now_playing?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
+let estrenos = "https://api.themoviedb.org/3/movie/upcoming?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
+let mejor_valoradas = "https://api.themoviedb.org/3/movie/top_rated?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
