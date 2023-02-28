96bf468c6cd272648acb410335bea162b693e3bd
rlogar <rociolopezgarcia2@gmail.com>
2023-02-28 20:36:02 +0100

    Intentando mostrar peliculas populares

--- js/peliculas.js
+++ js/peliculas.js
@@ -1,9 +1,30 @@
-let populares = "https://api.themoviedb.org/3/movie/popular?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
-let cartelera = "https://api.themoviedb.org/3/movie/now_playing?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
-let estrenos = "https://api.themoviedb.org/3/movie/upcoming?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
-let mejor_valoradas = "https://api.themoviedb.org/3/movie/top_rated?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
+const URL_IMG = "https://image.tmdb.org/t/p/w500"
+div_popu = document.querySelector("#container-populares");
+// div_cartelera = document.querySelector("#container-cartelera");
+// div_estrenos = document.querySelector("#container-estrenos");
+// div_top = document.querySelector("#container-top");
 
 //leemos populares
+const RecogePopulares = async () =>{
+    let populares = "https://api.themoviedb.org/3/movie/popular?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
+    const popular = await fetch(populares);
+    const data = await popular.json();
+    console.log(data);
 
-fetch(populares);
-const data = response.json();
+    div_popu.innerHTML ="";
+
+    data.results.map(item =>{
+        divItem = document.createElement('div');
+        divItem.innerHTML = `
+        <div class="pelicula">
+            <img class="imPeliTop" src="${URL_IMG+item.poster_path}" alt="...">
+            <div class="titulo">
+                <h2 class="titulo pelicula>${item.original_tittle}</h2>
+            </div>
+        </div>
+        `
+    }
+        )
+}
+
+RecogePopulares();
