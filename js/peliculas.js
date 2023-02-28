0b9ebbb72c5bb9c026c156d9f8bcb38c3cb95636
rlogar <rociolopezgarcia2@gmail.com>
2023-02-28 19:57:26 +0100

    Barra Navegación

--- js/peliculas.js
+++ js/peliculas.js
@@ -2,3 +2,8 @@ let populares = "https://api.themoviedb.org/3/movie/popular?api_key=24b37e513df8
 let cartelera = "https://api.themoviedb.org/3/movie/now_playing?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
 let estrenos = "https://api.themoviedb.org/3/movie/upcoming?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
 let mejor_valoradas = "https://api.themoviedb.org/3/movie/top_rated?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
+
+//leemos populares
+
+fetch(populares);
+const data = response.json();
