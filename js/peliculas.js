f6ea98d1a319459e2fec783016873eb3417039ef
rlogar <rociolopezgarcia2@gmail.com>
2023-03-01 12:56:22 +0100

    Recogiendo Peliculas populares de la API

--- js/peliculas.js
+++ js/peliculas.js
@@ -1,11 +1,11 @@
-const URL_IMG = "https://image.tmdb.org/t/p/w500";
+let URL_IMG = "https://image.tmdb.org/t/p/w500";
 div_popu = document.querySelector("#container-populares");
 div_cartelera = document.querySelector("#container-cartelera");
 div_estrenos = document.querySelector("#container-estrenos");
 //div_top = document.querySelector("#container-top");
 
 //leemos populares
-const RecogePopulares = async () =>{
+const recogePopulares = async () =>{
     let populares = "https://api.themoviedb.org/3/movie/popular?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
     const popular = await fetch(populares);
     const data = await popular.json();
@@ -17,60 +17,18 @@ const RecogePopulares = async () =>{
         divItem = document.createElement('div');
         divItem.innerHTML = `
         <div class="pelicula">
-            <img class="imPeliTop" src="${URL_IMG+item.poster_path}" alt="...">
-            <div class="titulo">
-                <h2 class="titulo pelicula>${item.original_tittle}</h2>
-            </div>
+                <h5 class="titulo">${item.original_title}</h5>
+                <img class="imgPopulares" src="${URL_IMG+item.poster_path}">
         </div>
         `
+        
+    div_popu.appendChild(divItem);
     }
     );
+    
+    
+    console.log("fuera del bucle");
 }
 
-//leemos populares
-const RecogeCartelera = async () =>{
-    let populares = "https://api.themoviedb.org/3/movie/now_playing?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
-    const popular = await fetch(populares);
-    const data = await popular.json();
-    console.log(data);
 
-    div_cartelera.innerHTML ="";
-
-    data.results.map(item =>{
-        divItem = document.createElement('div');
-        divItem.innerHTML = `
-        <div class="pelicula">
-            <img class="imPeliTop" src="${URL_IMG+item.poster_path}" alt="...">
-            <div class="titulo">
-                <h2 class="titulo pelicula>${item.original_tittle}</h2>
-            </div>
-        </div>
-        `
-    }
-    );
-}
-
-//leemos populares
-const RecogeEstrenos = async () =>{
-    let populares = "https://api.themoviedb.org/3/movie/upcoming?api_key=24b37e513df8dcb28d9c9e0abc3f5682";
-    const popular = await fetch(populares);
-    const data = await popular.json();
-    console.log(data);
-
-    div_estrenos.innerHTML ="";
-
-    data.results.map(item =>{
-        divItem = document.createElement('div');
-        divItem.innerHTML = `
-        <div class="pelicula">
-            <img class="imPeliTop" src="${URL_IMG+item.poster_path}" alt="...">
-            <div class="titulo">
-                <h2 class="titulo pelicula>${item.original_tittle}</h2>
-            </div>
-        </div>
-        `
-    }
-    );
-}
-
-RecogePopulares();
+recogePopulares();
